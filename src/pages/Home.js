445754b649f8009bea8f1fdbff54e77430395df3
Minesraft2 <aerellmck@gmail.com>
2022-07-26 16:30:32 -0500

    Add todo and some name fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 const Home = (props) => {
     return (<div className="homeMain">
-        <h1 className="homeTitle">Gimit</h1>
+        <h1 className="homeTitle">Gamit</h1>
         <div className="loginButtons">
             <Link to="/login"><button className="loginButton">Login</button></Link>
             <Link to="/signup"><button className="signupButton">Signup</button></Link>
