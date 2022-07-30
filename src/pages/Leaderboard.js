8be12a8b0dd7ef7945a3c4dcc7a9dbc6b9f28dd9
Minesraft2 <aerellmck@gmail.com>
2022-07-29 23:16:22 -0500

    Leaderboard state edits

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -17,8 +17,10 @@ const rtf = new Intl.RelativeTimeFormat("en", { style: "long" });
 const relatime = (elapsed) => {
     for (const [unit, amount] of UNITS) if (Math.abs(elapsed) > amount || unit === "second") return rtf.format(Math.round(elapsed / amount), unit);
 };
+const fakeUsers = new Array(50 - 3).fill(0).map(() => ({ username: Math.random().toFixed(9).slice(2, 9).split('').map(x => String.fromCharCode("a".charCodeAt(0) + parseInt(x))).join(''), "gam-bits": Math.floor(Math.random() * 6000), team: Math.round(Math.random()), createdAt: { seconds: (Date.now() / 1000) - Math.floor(Math.random() * 2628000) } })); // create 50 (minus 3 real current users) fake users
 const Leaderboard = (props) => {
     const [users, setUsers] = useState([]);
+    const [allUsers, setAllUsers] = useState([]);
     const [sort, setSort] = useState('');
     const [page, setPage] = useState(1);
 
@@ -28,14 +30,18 @@ const Leaderboard = (props) => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fakeUsers = new Array(50 - 3).fill(0).map(() => ({ username: Math.random().toFixed(9).slice(2, 9).split('').map(x => String.fromCharCode("a".charCodeAt(0) + parseInt(x))).join(''), "gam-bits": Math.floor(Math.random() * 6000), team: Math.round(Math.random()), createdAt: { seconds: (Date.now() / 1000) - Math.floor(Math.random() * 2628000) } })); // create 50 (minus 3 real current users) fake users
         getAllUsers() // grab the user database
-            .then(x => x
+            .then(users => setAllUsers(users
                 .concat(fakeUsers) // add fake users to list for some scroll space
                 .sort(({ "gam-bits": gambitsA }, { "gam-bits": gambitsB }) => gambitsB - gambitsA) // sort list by amount of bits they have
                 .map((x, i) => ({ ...x, rank: i + 1 })) // give each user a rank based on their place in the list
-            ).then(setUsers); // set users state to list
+            )) // set users state to list
     }, []);
+
+    useEffect(() => {
+        setUsers(allUsers.filter(({ team }) => params.get('filter') ? TEAMS[team] == TEAMS.find(x => x.toLowerCase() == params.get('filter')) : true)); // filter by team
+        setPage(1); // Reset page
+    }, [params.get('filter'), allUsers]);
 
     useEffect(() => {
         switch (sort) {
@@ -44,12 +50,16 @@ const Leaderboard = (props) => {
             case "username": setUsers([...users.sort((a, b) => a.username.toLowerCase() < b.username.toLowerCase() ? -1 : a.username.toLowerCase() > b.username.toLowerCase() ? 1 : 0)]); break;
             default: setUsers([...users.sort((a, b) => b[sort] - a[sort])]);
         }
+        setPage(1);
     }, [sort]);
 
     const handleSort = (Sort) => {
         if (Sort == sort) setUsers([...users.reverse()]);
         else setSort(Sort);
     }
+
+    window.page = () => setPage(oldPage => Math.min(oldPage + 1, Math.ceil(allUsers.length/15)));
+    window.getPage = () => page;
     return (
         <>
             <Nav {...props} />
@@ -57,8 +67,8 @@ const Leaderboard = (props) => {
             <div>
                 {
                     [0, 1].map((team) => {
-                        const totalBits = users.filter(x => x.team == team).map(x => x['gam-bits']).reduce((a, b) => a + b, 0);
-                        const winning = users.filter(x => x.team != team).map(x => x['gam-bits']).reduce((a, b) => a + b, 0) < totalBits;
+                        const totalBits = allUsers.filter(x => x.team == team).map(x => x['gam-bits']).reduce((a, b) => a + b, 0);
+                        const winning = allUsers.filter(x => x.team != team).map(x => x['gam-bits']).reduce((a, b) => a + b, 0) < totalBits;
                         return (
                             <div onClick={() => navigate(params.get('filter') === TEAMS[team].toLowerCase() ? `` : `?filter=${TEAMS[team].toLowerCase()}`)} className={`team${winning ? " winning" : ''}`} key={team}>
                                 <h2>{TEAMS[team]}</h2>
@@ -93,9 +103,7 @@ const Leaderboard = (props) => {
                     </thead>
                     <tbody>
                         {
-                            users.filter(({ team }) => {
-                                return params.get('filter') ? TEAMS[team] == TEAMS.find(x => x.toLowerCase() == params.get('filter')) : true
-                            }).slice(0, 15 * page).map(({ username, "gam-bits": gambits, team, createdAt: { seconds }, rank }) => {
+                            users.slice(0, 15 * page).map(({ username, "gam-bits": gambits, team, createdAt: { seconds }, rank }) => {
                                 return (
                                     <tr className={username == currentUser.displayName ? "active" : ''} key={rank}>
                                         <td>{rank}</td>
