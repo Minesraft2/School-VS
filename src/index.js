b917f94af78934b49b0e20edd907153740e4cdbc
Minesraft2 <aerellmck@gmail.com>
2022-08-01 10:34:54 -0500

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import App from './App.js';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 
-setTimeout(() => createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>), 0);
+createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>);
